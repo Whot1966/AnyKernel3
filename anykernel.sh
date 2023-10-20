ff6e7cfb70542accd32bb74a4aef4a89457b5405
Whot1966 <Whot1966@users.noreply.github.com>
2023-10-20 13:14:59 +0000

    AK3: Rebrand to Neko

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=QuicksilveR kernel with KernelSU supported
+kernel.string=Neko kernel with KernelSU supported
 do.devicecheck=1
 do.modules=0
 do.systemless=1
